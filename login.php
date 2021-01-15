0bde3b86c0f4eba24e539c885945649cf08ef6bb
Vasu <44114066+vasumsv@users.noreply.github.com>
2021-01-15 22:14:19 +0530

    new updated

---
+++ login.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html  >
+<head>
+   
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v5.1.4, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo5.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  
+  <title>Login</title>
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+  
+</head>
+<body>
+  
+  <section class="content11 cid-sjMJ4mnTMZ" id="content11-5">
+    
+    <style>
+#box {
+  background-color: white;
+  width:600px;
+  padding: 50px;
+  margin: 20px;
+}
+
+input[type=text], input[type=password] {
+
+  /*  width: 60%;*/
+  /*padding: 12px 20px;*/
+  /*margin: 8px 0;*/
+  /*display: inline-block;*/
+  /*border: 1px solid #ccc;*/
+  /*box-sizing: border-box;*/
+  
+  width: 90%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid grey;
+}
+
+button {
+  background-color: #6ec7f2;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 50%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+.imgcontainer {
+  text-align: center;
+  margin: 12px 0 12px 0;
+}
+
+img.avatar {
+  width: 40%;
+  
+}
+.imgcenter {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+.contentcenter {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+#logtext
+{
+    font-family:"arial";
+}
+</style>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 col-lg-6">
+                 <div id="box"> 
+                 <div class="imgcontainer">
+    <img src="assets/images/1608526896930.png" alt="Avatar" class="imgcenter">
+     <label class="contentcenter" id="logtext"><strong>Log into your Account  </strong></label>
+  </div>
+  
+        <div class="container">
+            <div class="login-box">
+        <form action="loginvalidation.php" method="post">
+            <label  ><strong>Username </strong></label> 
+                <input type="text" placeholder="Enter Username" class="contentcenter" name="user" required="">
+                 <br>
+                
+           
+            <label  ><strong>Password  </strong></label>
+                 <input type="password" placeholder="Enter Password" class="contentcenter" name="password" required="">
+            
+            <button type="submit" class="contentcenter">Login</button>
+            </form>
+            <label>
+                 <input type="checkbox" checked="" name="remember"> Remember me
+            </label><br>
+            <label ><u>forgot password?</u></label>
+            <br>
+            <strong>New User?&nbsp;</strong><a href="register.php"><label >Sign Up</p></label>   </a>
+          </form>
+          </div>
+</div>
+                 
+                 
+                 </div>
+
+            </div>
+        </div>
+    </div>
+</section> <script src="assets/web/assets/jquery/jquery.min.js"></script>  <script src="assets/popper/popper.min.js"></script>  <script src="assets/tether/tether.min.js"></script>  <script src="assets/bootstrap/js/bootstrap.min.js"></script>  <script src="assets/smoothscroll/smooth-scroll.js"></script>  <script src="assets/theme/js/script.js"></script>  
+  
+  
+</body>
+</html>
---
+++ registervalidation.php
@@ -0,0 +1,27 @@
+<?php 
+            session_start();
+            header('location:login.php');
+
+            $con = mysqli_connect('localhost','root','');
+
+            mysqli_select_db($con,'electronics');
+            $name = $_POST['user'];
+            // $pass = md5($_POST['password']);
+            $pass = $_POST['password'];
+
+            $s = "select * from usertable where name = '$name'";
+
+            $result = mysqli_query($con,$s);
+
+            $num = mysqli_num_rows($result);
+
+            if($num == 1)
+            {
+                echo"username Already Taken";
+            }
+            else{
+                $reg = "INSERT INTO usertable(name, password) values ('$name','$pass')";
+                mysqli_query($con,$reg);
+                echo "$name your registeration is sucessful";
+            }
+        ?>
